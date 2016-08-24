e4a8c8beb387bda96aeb377becfb4c43ba1a18ce
Sven Jonsson <sigsjo@dbwebb.se>
2016-08-24 16:48:31 +0100

    init

---
+++ tva.py
@@ -0,0 +1,11 @@
+def add(a,b):
+    c = a + b
+    return c
+	
+	
+print("Hello")
+# comment
+i = int(input("Type in a number , and it will be repeated on screen: "))
+print (i)
+i = add(i,1)
+print (i)
